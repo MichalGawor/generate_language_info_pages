37728c78f7b87c945514ef01a8903132902b7e00
MichalGawor <michal.j.gawor@gmail.com>
2020-03-05 17:42:23 +0100

    push generated info pages in directory

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -11,9 +11,8 @@ bash ./docker-run.sh ./lang_info_pages
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "travis" ]; then exit 0; fi
 
 # deploy
-cd lang_info_pages
 git init
-git add .
+git add ./lang_info_pages
 git commit -m "Deploy lang info pages"
 
 git push --force https://${GITHUB_TOKEN}:x-oauth-basic@github.com/clarin-eric/generate_language_info_pages.git HEAD:lang-info-pages
