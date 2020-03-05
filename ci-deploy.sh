4ed0b815e2e1b7de5d5676761f52a2a37e4f502f
MichalGawor <michal.j.gawor@gmail.com>
2020-03-05 15:40:13 +0100

    ci-deploy.sh

---
+++ ci-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+set -e
+
+# generate language info pages
+bash ./docker-run.sh ./lang_info_pages
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "travis" ]; then exit 0; fi
+
+# deploy
+cd lang_info_pages
+git init
+git add .
+git commit -m "Deploy lang info pages"
+
+git push --force https://${GITHUB_TOKEN}:x-oauth-basic@github.com/clarin-eric/generate_language_info_pages.git HEAD:lang-info-pages
