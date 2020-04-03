cde0d201734c8d94dd5fa0918523917d8a9ea44e
Travis CI <>
2020-04-03 20:33:44 +0200

    echo TRAVIS_TAG before info pages generation

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
 set -e
+
+echo "Travis tag: $TAVIS_TAG"
 
 # generate language info pages
 bash ./docker-run.sh ./lang_info_pages
@@ -13,6 +11,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then e
 # deploy generated pages to origin/lang-info-pages
 
 # create tarball with generated language info pages
-tar -cvfz lang_info_pages ./lang_info_pages/
+tar -cvfz lang_info_pages-${LANG_INFO_PAGES_RAG} ./lang_info_pages/
 
 echo "Travis tag: $TAVIS_TAG"
