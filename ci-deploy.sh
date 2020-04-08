6aa97924c32be72b3df4dfea8890222d86e4a66e
Travis CI <>
2020-04-08 13:45:07 +0200

    tar before condition for pull request

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -6,6 +6,8 @@ echo "LANG_INFO_PAGES_TAG: ${LANG_INFO_PAGES_TAG}"
 
 # generate language info pages
 bash ./docker-run.sh ./lang_info_pages
+tar -czf lang_info_pages-${LANG_INFO_PAGES_TAG} ./lang_info_pages/
+
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
