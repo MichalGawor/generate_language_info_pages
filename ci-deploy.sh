0a320f0f5f82726cf1a3a5ba6a171b5263c98cc1
Travis CI <>
2020-04-14 17:14:02 +0200

    deploy release

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -6,14 +6,11 @@ echo "Lang pages: ${LANGUAGE_PAGES}"
 
 # generate language info pages
 bash ./docker-run.sh ./lang_info_pages
-tar -czf lang_info_pages./lang_info_pages/
+tar -czf lang_info_pages ./lang_info_pages/
 
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
-# deploy generated pages to origin/lang-info-pages
-
 # create tarball with generated language info pages
 tar -czf lang_info_pages ./lang_info_pages/
 
-echo "Travis tag: ${TAVIS_TAG}"
