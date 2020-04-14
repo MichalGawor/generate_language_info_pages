ce8d0db2b926d143c20d2dfdfc02b51073d12aa7
Travis CI <>
2020-04-14 16:16:56 +0200

    try to echo travis env var

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Travis tag: ${TAVIS_TAG}"
-echo "LANG_INFO_PAGES_TAG: ${LANG_INFO_PAGES_TAG}"
+echo "Travis tag: (${TAVIS_TAG})"
+echo "Lang pages: ${LANGUAGE_PAGES}"
 
 # generate language info pages
 bash ./docker-run.sh ./lang_info_pages
-tar -czf lang_info_pages-${LANG_INFO_PAGES_TAG} ./lang_info_pages/
+tar -czf lang_info_pages./lang_info_pages/
 
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
@@ -14,6 +14,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then e
 # deploy generated pages to origin/lang-info-pages
 
 # create tarball with generated language info pages
-tar -czf lang_info_pages-${LANG_INFO_PAGES_TAG} ./lang_info_pages/
+tar -czf lang_info_pages ./lang_info_pages/
 
 echo "Travis tag: ${TAVIS_TAG}"
