b82b02d1ac1acf3753b2043d94b6d526336188e6
Travis CI <>
2020-04-15 19:06:13 +0200

    tar after branch condition

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -5,10 +5,11 @@ echo "Lang pages: ${LANGUAGE_PAGES_VERSION}"
 
 # generate language info pages
 bash ./docker-run.sh ./lang_info_pages
-tar czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
-
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+tar czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
+
 
 # create tarball with generated language info pages
 
