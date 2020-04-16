c631ad7fa64d62c8fcfef6a663173fa5dcec28a0
Travis CI <>
2020-04-16 17:37:52 +0200

    compressed results found locally

--- ci-pre-deploy.sh
+++ ci-pre-deploy.sh
@@ -1,10 +1,11 @@
 set -e
 
 echo "Compress results"
-tar czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
+tar -czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
 
-if [[ -f "lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz" ]]
-then
+echo "lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz"
+
+if [ -e ./lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz ]; then
   echo "Compressed results lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz finished"
 else
   echo "Failed to compress results"
