5799fd1372734639d438cb19cda4389a819eebd2
Travis CI <>
2020-04-16 16:47:08 +0200

    compression start end echo

--- ci-pre-deploy.sh
+++ ci-pre-deploy.sh
@@ -1,3 +1,5 @@
 set -e
 
+echo "Compress results"
 tar czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
+echo "Compressed results lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz finished"
