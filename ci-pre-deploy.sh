127e60a9ee8780f10dcbe53a4238e09ac9c9061d
Travis CI <>
2020-04-16 16:55:02 +0200

    compression verbosity

--- ci-pre-deploy.sh
+++ ci-pre-deploy.sh
@@ -2,4 +2,10 @@ set -e
 
 echo "Compress results"
 tar czvf lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz lang_info_pages/
-echo "Compressed results lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz finished"
+
+if [[ -f "lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz" ]]
+then
+  echo "Compressed results lang_info_pages_${LANGUAGE_PAGES_VERSION}.tar.gz finished"
+else
+  echo "Failed to compress results"
+fi
