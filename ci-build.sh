27afb415175322925f0f27051cff9c62afdc569c
Travis CI <>
2020-04-23 14:19:42 +0200

    delete spaces in env var assignment

--- ci-build.sh
+++ ci-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "${TRAVIS_TAG}" ]; then
-		echo "TRAVIS_TAG variable not set, skipping build"
-		exit 0
-	else
-		echo "Tag: ${TRAVIS_TAG}"
-fi
+#if [ -z "${TRAVIS_TAG}" ]; then
+#		echo "TRAVIS_TAG variable not set, skipping build"
+#		exit 0
+#	else
+#		echo "Tag: ${TRAVIS_TAG}"
+#fi
 
 echo "Generating language info pages"
 bash ./docker-run.sh ./lang_info_pages
--- ci-pre-deploy.sh
+++ ci-pre-deploy.sh
@@ -1,17 +1,17 @@
 set -e
 
-if [ -z "${TRAVIS_TAG}" ]; then
-		echo "TRAVIS_TAG variable not set, skipping deployment"
-		exit 0
-	else
-		echo "Tag: ${TRAVIS_TAG}"
-fi
+#if [ -z "${TRAVIS_TAG}" ]; then
+#		echo "TRAVIS_TAG variable not set, skipping deployment"
+#		exit 0
+#	else
+#		echo "Tag: ${TRAVIS_TAG}"
+#fi
 
 echo "Compress results"
 tar -czvf ${LANG_INFO_PAGES_TAR} lang_info_pages/
 
 if [ -e ./${LANG_INFO_PAGES_TAR} ]; then
-  echo "Compressed results lang_info_pages_${LANG_INFO_PAGES_TAR}.tar.gz finished"
+  echo "Compressed results ${LANG_INFO_PAGES_TAR} finished"
 else
   echo "Failed to compress results"
 fi
