f23c734b25736ccc94ac8434061b7fb560d829f0
Travis CI <>
2020-04-23 15:52:46 +0200

    test build

--- ci-pre-deploy.sh
+++ ci-pre-deploy.sh
@@ -1,12 +1,5 @@
 set -e
 
-if [ -z "${TRAVIS_TAG}" ]; then
-		echo "TRAVIS_TAG variable not set, skipping deployment"
-		exit 0
-	else
-		echo "Tag: ${TRAVIS_TAG}"
-fi
-
 echo "Compress results"
 tar -czvf ${LANG_INFO_PAGES_TAR} lang_info_pages/
 
